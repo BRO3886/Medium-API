fec5dde21dfef8a0df7ac75ec38efff6816d6773
Siddhartha Varma <siddverma1999@gmail.com>
2020-10-22 15:08:26 +0530

    better err handling

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import xmltodict
 from typing import Optional
 from pydantic import BaseModel
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -19,9 +19,12 @@ def parse_data(uri:str):
     'Cookie': '__cfduid=dbdade35f2ca26435e1b04fb37a20c77b1601388967; uid=lo_2676c4ef2585; __cfruid=94c564f54a6364cc766586f4a13965b29547ab9f-1601388968'
   }
   
-  response = requests.request("GET", uri, headers=headers, data = payload)
-  
-  data = xmltodict.parse(response.text.encode('utf8'))
+  try:
+    response = requests.request("GET", uri, headers=headers, data = payload)
+    data = xmltodict.parse(response.text.encode('utf8'))  
+  except Exception as e:
+    raise HTTPException(status_code=400, detail="unable to find that link or parse it")
+
 
   l = []
   for k in data['rss']['channel']['item']:
@@ -51,3 +54,4 @@ def get_data(uri:Uri):
 @app.get("/health")
 def get_health():
   return {"ping":"Its working fast 🚅"}
+  
